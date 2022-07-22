c19045a75a6911478235fcacad1b0227f8310a11
Irtiza Batool <irtizabatool@gmail.com>
2022-07-22 11:23:35 +0530

    deleted yarn lock file

--- src/shared/constants/strings.ts
+++ src/shared/constants/strings.ts
@@ -6,8 +6,11 @@ export const EMPTY_FIELD = 'field should not be empty!';
 
 export const STRING_VALUE = 'field should be a string value!';
 
-export const PASSWORD_REGEX = /^(?:(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/;
-   
-export const MATCH_PASSWORD = 'Password should be atleast 8 characters and should contain combination of uppercase, lowercase & numbers!';
+export const PASSWORD_REGEX =
+  /^(?:(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/;
 
-export const PASSWORD_LENGTH = 'Passwords should not be less than 8 characters!';
+export const MATCH_PASSWORD =
+  'Password should be atleast 8 characters and should contain combination of uppercase, lowercase & numbers!';
+
+export const PASSWORD_LENGTH =
+  'Passwords should not be less than 8 characters!';
